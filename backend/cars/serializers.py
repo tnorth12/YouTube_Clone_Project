8e2d8e03ad8bb7c675231bbef3e7f73b2fb3d773
Your Name <you@example.com>
2022-09-05 16:31:08 -0600

    updated user_ID field in serializer.

--- backend/cars/serializers.py
+++ backend/cars/serializers.py
@@ -7,5 +7,8 @@ from .models import Car
 class CarSerializer(serializers.ModelSerializer):
     class Meta:
         model = Car
-        fields = ['id', 'make', 'model', 'year', 'user_id']
+        fields = ['id', 'make', 'model', 'year', 'user']        
         depth = 1
+
+
+# user_id
