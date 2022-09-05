aa5368b976b8a1deff210a9be701da718dfb143e
Your Name <you@example.com>
2022-09-04 19:13:59 -0600

    Setup local_setting file and added database/secret_key entries + created youtube_project database + installed dependencies.

--- backend/drf_jwt_backend/settings.py
+++ backend/drf_jwt_backend/settings.py
@@ -95,6 +95,17 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+SECRET_KEY = 'django-insecure-$^kgo(+w%j55*igm==55a#tp7!!e7f)cxi=7w7-q*#85x7yfw5'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'youtube_project',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'Noba2i7w@jUg'
+    }
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
